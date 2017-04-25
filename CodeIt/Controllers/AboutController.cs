39b77bab5a07355c9ec3d7e92bc551277abe89fd
atanasovMartin <atanasov.martin@abv.bg>
2017-04-25 15:29:52 -0700

    add Views/About/About.cshtml, Controllers/AboutController.cs

---
+++ CodeIt/Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace CodeIt.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+        
+    }
+}
